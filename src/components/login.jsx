ef4c392a9338a9f4ca7c267148a15d0d0f866cab
Arjun P <arjunp@jmangroup.com>
2024-03-27 19:22:37 +0530

    feat: init

---
+++ src/components/login.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      
+      const res = await response.json();
+      console.log(res)
+
+      if (res.message == 'Username or password incorrect') {
+        alert('Invalid email or password');
+      }
+      else{
+        const { accessToken,role } = res;
+        console.log(accessToken,role,email);
+        sessionStorage.setItem('accessToken', accessToken);
+        sessionStorage.setItem('role', role);
+        sessionStorage.setItem('email',email);
+      }
+      
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        {error && <p className="error">{error}</p>}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/register.jsx
@@ -0,0 +1,148 @@
+import React, { useState,useEffect } from 'react';
+
+function RegisterUser() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('');
+  const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [error, setError] = useState('');
+  const [userRole, setUserRole] = useState('');
+
+
+  useEffect(() => {
+    setUserRole(sessionStorage.getItem('role'));
+    console.log(userRole);
+  }, []);
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handleRoleChange = (event) => {
+    setRole(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+
+
+      if (userRole !== 'admin') {
+        throw new Error('Only admins can register new users');
+      }
+
+      const response = await fetch('http://localhost:3000/api/registerUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`
+        },
+        body: JSON.stringify({ first_name:firstName, last_name:lastName, role:role, email:email, password:password, phone_number:phoneNumber }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to register user');
+      }
+
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      {userRole === 'admin' ? ( 
+        <div>
+            <h1>Register New User</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={handleFirstNameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={handleLastNameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="role">Role:</label>
+          <input
+            type="text"
+            id="role"
+            value={role}
+            onChange={handleRoleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            type="text"
+            id="phoneNumber"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+            required
+          />
+        </div>
+        {error && <p>{error}</p>}
+        <button type="submit">Register</button>
+      </form>
+        </div>
+      ) : (
+        <p>You are not permitted to register new users.</p>
+      )}
+    </div>
+  );
+}
+
+export default RegisterUser;
